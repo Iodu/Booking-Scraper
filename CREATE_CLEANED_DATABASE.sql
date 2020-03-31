b11e25f3a479d171b06ebbbdd5708a4e309dcaf9
Iodu <48859312+Iodu@users.noreply.github.com>
2020-03-31 13:03:45 +0200

    Delete CREATE_CLEANED_DATABASE.sql

--- CREATE_CLEANED_DATABASE.sql
+++
@@ -1,8 +0,0 @@
-CREATE DATABASE IF NOT EXISTS Hotel_reviews;
-USE Hotel_reviews;
-CREATE TABLE IF NOT EXISTS cleaned(
-  id BIGINT NOT NULL AUTO_INCREMENT,
-  Negative_Review TEXT,
-  Positive_Review TEXT,
-  PRIMARY KEY (id)
-);
